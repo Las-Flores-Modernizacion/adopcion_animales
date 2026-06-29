631f8bd003018a93310ea9408cb1f12b9be3360a
agustin herrera <agustinherrera.dev@gmail.com>
2026-06-29 15:02:30 -0300

    Se añade un campo photo y una variable thumb para guardar una imagen por cada reporte

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,2 +1,7 @@
 class Report < ApplicationRecord
+  has_one_attached :photo do |attachable|
+  attachable.variant :thumb, resize_to_limit: [ 300, 300 ], saver: { quality: 80 }, format: :webp, preprocessed: true
+  end
+
+  validates :photo, presence: true
 end
