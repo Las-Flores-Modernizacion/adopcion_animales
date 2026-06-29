51413084cf48121bfcb55b801116ba5f94b02b7e
Tomás Urra <urratomi002@gmail.com>
2026-06-29 13:02:46 +0000

    Creación de validación, normalización y testing para asnwer_to_name, race y unique_detail
    Combine la normalización de las 3 variables y color. Hice que la validación de answer_to_name y race acepten solo letras y 40 caracteres máximos y esto último tambien en unique_detail. Se realizó un testing para uno

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -3,10 +3,22 @@ class Animal < ApplicationRecord
   enum :species, { perro: 0, gato: 1 }
 
   # Normalizaciones
-  normalizes :color, with: ->(color) { color.strip.downcase }
+  normalizes :color, :answer_to_name, :race, :unique_detail,  with: ->(colNameRaceDetail) { colNameRaceDetail.strip.downcase }
 
   # Validaciones
   validates :color,
     format: { with: /\A[a-zA-ZáéíóúÁÉÍÓÚñÑüÜ\s]+\z/, message: "Solo se permiten letras." },
     length: { minimum: 3, maximum: 40, message: "Solo se permiten colores con más de 3 letras y menos de 41" }
+
+  validates :answer_to_name,
+    format: { with: /\A[a-zA-ZáéíóúÁÉÍÓÚñÑüÜ\s]+\z/, message: "Solo se permiten letras." },
+    length: { maximum: 40, message: "Como máximo puedes escribir 40 carácteres"}
+    
+  validates :race,
+    format: { with: /\A[a-zA-ZáéíóúÁÉÍÓÚñÑüÜ\s]+\z/, message: "Solo se permiten letras." },
+    length: { maximum: 40, message: "Como máximo puedes escribir 40 carácteres"}
+  
+  validates :unique_detail,
+    length: { maximum: 40, message: "Como máximo puedes escribir 40 carácteres"}
+
 end
--- test/models/animal_test.rb
+++ test/models/animal_test.rb
@@ -16,4 +16,48 @@ class AnimalTest < ActiveSupport::TestCase
     animal.color = "marrón"
     assert animal.save
   end
+
+  test "el nombre debe ser solo letras" do
+
+    animal = @tito
+    animal.answer_to_name = "1234567890¡?'¿+{}-.,;:#^`~!$%&/()°|¬"
+    assert_not animal.save
+  
+    animal.answer_to_name = "     toto1        "
+    assert_not animal.save
+
+    animal.answer_to_name = "toto 1     "
+    assert_not animal.save
+
+    animal.answer_to_name = "toto   "
+    assert animal.save
+
+  end
+
+  test "la raza solo puede tener letras" do
+
+    animal = @tito
+    animal.race = "1234567890¡?'¿+{}-.,;:#^`~!$%&/()°|¬"
+    assert_not animal.save
+  
+    animal.race = "     caniche112        "
+    assert_not animal.save
+
+    animal.race = "caniche 1     "
+    assert_not animal.save
+
+    animal.race = "Caniche   "
+    assert animal.save
+
+  end
+
+  test "máximos caracteres en detalle único" do
+
+    animal = @tito
+    animal.unique_detail = "El perrito tenia una pequeña lastimadura en la patita derecha"
+    assert_not animal.save
+
+    animal.unique_detail = "Estaba renguito"
+    assert animal.save
+  end
 end
