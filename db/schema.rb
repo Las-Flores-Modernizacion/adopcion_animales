2628d36374c625cf0c44dfd6d74626ddd98bca39
agustin herrera <agustinherrera.dev@gmail.com>
2026-06-29 15:13:21 -0300

    Se añadio la referencia user a la tabla accounts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2026_06_29_181002) do
+ActiveRecord::Schema[8.0].define(version: 2026_06_29_181215) do
   create_table "accounts", force: :cascade do |t|
     t.string "email_address", null: false
     t.string "password_digest"
@@ -22,7 +22,9 @@ ActiveRecord::Schema[8.0].define(version: 2026_06_29_181002) do
     t.string "oauth_expires_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
     t.index ["email_address"], name: "index_accounts_on_email_address", unique: true
+    t.index ["user_id"], name: "index_accounts_on_user_id"
   end
 
   create_table "active_storage_attachments", force: :cascade do |t|
@@ -96,6 +98,7 @@ ActiveRecord::Schema[8.0].define(version: 2026_06_29_181002) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "accounts", "users"
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "sessions", "accounts"
