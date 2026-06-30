a1cbd10ca26e2062daf48dbbdd9dc1c29d25bae9
agustin herrera <agustinherrera.dev@gmail.com>
2026-06-30 12:06:38 -0300

    Limpieza de espacios en blanco en models/animal.rb

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,27 +1,25 @@
 class Animal < ApplicationRecord
   enum :size, { pequeño: 0, mediano: 1, grande: 2 }
   enum :species, { perro: 0, gato: 1 }
-  
+
   # Normalizaciones
-  normalizes :color, :answer_to_name, :race, :unique_detail,  with: ->(colNameRaceDetail) { colNameRaceDetail.strip.downcase }
+  normalizes :color, :answer_to_name, :race, :unique_detail,  with: ->(e) { e.strip.downcase }
 
   # Validaciones
   validates :color,
     format: { with: /\A[a-zA-ZáéíóúÁÉÍÓÚñÑüÜ\s]+\z/, message: "Solo se permiten letras." },
     length: { minimum: 3, maximum: 40, message: "Solo se permiten colores con más de 3 letras y menos de 41" }
 
-  
   validates :age, numericality: { only_integer: true, less_than_or_equal_to: 25 }
 
   validates :answer_to_name,
     format: { with: /\A[a-zA-ZáéíóúÁÉÍÓÚñÑüÜ\s]+\z/, message: "Solo se permiten letras." },
-    length: { maximum: 40, message: "Como máximo puedes escribir 40 carácteres"}
-    
+    length: { maximum: 40, message: "Como máximo puedes escribir 40 carácteres" }
+
   validates :race,
     format: { with: /\A[a-zA-ZáéíóúÁÉÍÓÚñÑüÜ\s]+\z/, message: "Solo se permiten letras." },
-    length: { maximum: 40, message: "Como máximo puedes escribir 40 carácteres"}
-  
+    length: { maximum: 40, message: "Como máximo puedes escribir 40 carácteres" }
   validates :unique_detail,
-    length: { maximum: 40, message: "Como máximo puedes escribir 40 carácteres"}
 
-end 
+    length: { maximum: 40, message: "Como máximo puedes escribir 40 carácteres" }
+end
