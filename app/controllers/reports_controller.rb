608d6665f995a9b00dad3f43f4df6351ceeea07b
agustin herrera <agustinherrera.dev@gmail.com>
2026-06-30 12:39:29 -0300

    Añade views/action controller basicos para crear y visualizar reportes

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,3 +1,28 @@
 class ReportsController < ApplicationController
-  def index; end
+  def index
+    @reports = Current.user.reports
+  end
+
+  def new
+    @report = Report.new
+  end
+
+  def create
+    @report = Current.user.reports.new(claims_params)
+    if @report.save
+      redirect_to reports_path, notice: "Reporte creado exitosamente."
+    else
+      render :new, status: :unprocessable_content, alert: "Error al publicar reporte, intenta de nuevo por favor."
+    end
+  end
+
+  private
+
+  def set_report; end
+
+  def claims_params
+    params.require(:report).permit(
+      :photo
+    )
+  end
 end
---
+++ app/javascript/controllers/image_compression_controller.js
@@ -0,0 +1,28 @@
+import { Controller } from "@hotwired/stimulus"
+import Compressor from "compressorjs"
+
+export default class extends Controller {
+  static targets = [ "input", "info" ]
+
+  compress(event) {
+    const file = event.target.files[0];
+    if (!file) return;
+
+    this.infoTarget.textContent = `Procesando: ${file.name}...`
+    this.infoTarget.classList.add("text-blue-500")
+
+    new Compressor(file, {
+      quality: 0.6, // Baja la calidad al 60%
+      maxWidth: 1920, // Limita el ancho máximo (ej. Full HD)
+      success: (compressedResult) => {
+        // Truco para reemplazar el archivo pesado por el ligero en el input
+        let dataTransfer = new DataTransfer();
+        dataTransfer.items.add(compressedResult);
+        this.inputTarget.files = dataTransfer.files;
+      },
+      error(err) {
+        console.log(err.message);
+      },
+    });
+  }
+}
