0e8b368f6984c751a6b55cf4c3ebd8d33ee7e1b6
agustin herrera <agustinherrera.dev@gmail.com>
2026-06-30 12:37:40 -0300

    Añade routes para reports y se configura pages#home como root route global

--- config/routes.rb
+++ config/routes.rb
@@ -11,10 +11,12 @@ Rails.application.routes.draw do
   end
   delete "/logout", to: "sessions#destroy"
 
+  resources :reports, path: "reportes"
+
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
-  root "reports#index"
+  root "pages#home"
 end
