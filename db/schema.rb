b39e8a15baa151aa3edfe4795737a4d29bce78ae
agustin herrera <agustinherrera.dev@gmail.com>
2026-07-07 11:18:19 -0300

    feat(report): agrega referencia obligatoria a location en reports
    Crea la migración para añadir `location_id` a la tabla de reportes con
    restricción de no nulidad y clave foránea. Actualiza el esquema de la
    base de datos de manera correspondiente.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2026_06_30_131312) do
+ActiveRecord::Schema[8.0].define(version: 2026_07_07_132851) do
   create_table "accounts", force: :cascade do |t|
     t.string "email_address", null: false
     t.string "password_digest"
@@ -83,6 +83,8 @@ ActiveRecord::Schema[8.0].define(version: 2026_06_30_131312) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id", null: false
+    t.integer "location_id", null: false
+    t.index ["location_id"], name: "index_reports_on_location_id"
     t.index ["user_id"], name: "index_reports_on_user_id"
   end
 
@@ -103,6 +105,7 @@ ActiveRecord::Schema[8.0].define(version: 2026_06_30_131312) do
   add_foreign_key "accounts", "users"
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "reports", "locations"
   add_foreign_key "reports", "users"
   add_foreign_key "sessions", "accounts"
 end
