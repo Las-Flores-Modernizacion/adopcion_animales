5725637e2538d54b50bf9bf8346b6efe96962648
Tomás Urra <urratomi002@gmail.com>
2026-07-07 13:49:04 +0000

    Creación de testing de reportes
    Se testea que el reporte sea válido, que tenga una foto adjunta, los formatos válidos del archivo adjuntado en el reporte y el tamaño

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -1,7 +1,60 @@
 require "test_helper"
 
 class ReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @reporte = reports(:reporte)
+  end
+
+  test "el reporte es válido" do # Podria no hacerlo
+    @reporte.photo.attach(
+      io: file_fixture("colibri estatico.png").open,
+      filename: "prueba",
+      content_type: "image/png"
+    )
+    assert @reporte.valid?, @reporte.errors.full_messages
+  end
+
+  test "el reporte tiene una foto adjunta?" do
+    reporte = @reporte
+    reporte.photo.attach(
+      io: file_fixture("colibri estatico.png").open,
+      filename: "prueba",
+      content_type: "image/png"
+    )
+    assert reporte.photo.attached?
+
+    reporte.photo.purge
+    assert_not reporte.photo.attached?
+  end
+
+  test "solo se aceptan formatos válidos en los archivos" do
+    reporte = @reporte
+    reporte.photo.attach(
+      io: file_fixture("Apropiación artística.docx").open,
+      filename: "prueba2",
+      content_type: "application/pdf"
+    )
+    assert_not reporte.save
+    # reporte.errors[:photo]
+
+    reporte = @reporte
+    reporte.photo.attach(
+      io: file_fixture("colibri estatico.png").open,
+      filename: "prueba",
+      content_type: "image/png"
+    )
+    assert reporte.save
+  end
+
+  test "el tamaño del archivo debe ser menor a 20 MegaBytes" do
+    reporte = @reporte
+    reporte.photo.attach(
+      io: file_fixture("colibri estatico.png").open,
+      filename: "prueba",
+      content_type: "image/png"
+    )
+
+    # p reporte.photo.blob.byte_size.megabytes
+    assert reporte.save
+  end
 end
