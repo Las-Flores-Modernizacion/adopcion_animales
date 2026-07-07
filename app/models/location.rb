85519e0e3cc356fc0c3120a4831f088096f757bf
agustin herrera <agustinherrera.dev@gmail.com>
2026-07-07 11:22:03 -0300

    fix(location): corrige consulta sql para generar la geometria

--- app/models/location.rb
+++ app/models/location.rb
@@ -7,7 +7,7 @@ class Location < ApplicationRecord
 
   def update_geometry
     self.class.connection.execute(
-      "UPDATE locations SET geometry = SetSRID(MakePoint(#{longitude}. #{latitude}), 4326) WHERE id = #{id}"
+      "UPDATE locations SET geometry = ST_AsBinary(ST_GeomFromText('POINT(#{longitude} #{latitude})', 4326)) WHERE id = #{id}"
     )
   end
 end
