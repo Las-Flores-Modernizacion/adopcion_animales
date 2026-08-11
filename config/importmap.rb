92d85aa0cd4e90dd3bbea0db964e3e3a0790c3d7
agustin herrera <agustinherrera.dev@gmail.com>
2026-08-11 11:59:07 -0300

    build(javascript): inicializa ActiveStorage y fija dependencias en importmap

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,4 +5,7 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "@rails/activestorage", to: "activestorage.esm.js"
 pin "compressorjs" # @1.3.0
+pin "embla-carousel", to: "https://cdn.jsdelivr.net/npm/embla-carousel/embla-carousel.esm.js"
+pin "embla-carousel-wheel-gestures", to: "https://cdn.jsdelivr.net/npm/embla-carousel-wheel-gestures@latest/+esm"
